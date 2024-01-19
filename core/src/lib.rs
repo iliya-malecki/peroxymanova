5760dbd9bbba53956fa3004ee0056d3141ffd710
Iliya Malecki <iliyamalecki@gmail.com>
2024-01-19 01:48:10 +0100

    added a data generator for later tasting and benching

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,29 +1,28 @@
-use numpy::{Ix2, PyReadonlyArray};
+use ndarray::prelude::*;
+use ndarray_rand::rand_distr::Uniform;
+use ndarray_rand::RandomExt;
 use rand::seq::SliceRandom;
+
 // use polars;
 
-pub fn get_ss_t(sqdistances: &PyReadonlyArray<f64, Ix2>) -> f64 {
+fn get_ss_t(sqdistances: &Array<f64, Ix2>) -> f64 {
     let mut sum = 0f64;
     for i in 0..sqdistances.shape()[0] {
         for j in 0..sqdistances.shape()[1] {
             if i != j {
-                sum += sqdistances.get([i, j]).unwrap();
+                sum += sqdistances[[i,j]];
             }
         }
     }
     return sum / (sqdistances.shape()[0] as f64) / 2.;
 }
 
-pub fn get_ss_w(
-    sqdistances: &PyReadonlyArray<f64, Ix2>,
-    labels: &Vec<usize>,
-    bincount: &Vec<i64>,
-) -> f64 {
+fn get_ss_w(sqdistances: &Array<f64, Ix2>, labels: &Vec<usize>, bincount: &Vec<i64>) -> f64 {
     let mut sums = vec![0f64; bincount.len()];
     for i in 0..sqdistances.shape()[0] {
         for j in 0..sqdistances.shape()[0] {
             if labels[i] == labels[j] && i != j {
-                sums[labels[i]] += sqdistances.get([i, j]).unwrap();
+                sums[labels[i]] += sqdistances[[i,j]];
             }
         }
     }
@@ -36,12 +35,12 @@ pub fn get_ss_w(
     sum / 2.
 }
 
-pub fn get_f(ss_t: f64, ss_w: f64, a: i64, n: i64) -> f64 {
+fn get_f(ss_t: f64, ss_w: f64, a: i64, n: i64) -> f64 {
     let ss_a = ss_t - ss_w;
     (ss_a / (a - 1) as f64) / (ss_w / (n - a) as f64)
 }
 
-pub fn permanova(sqdistances: PyReadonlyArray<f64, Ix2>, mut labels: Vec<usize>) -> (f64, f64) {
+pub fn permanova(sqdistances: Array<f64, Ix2>, mut labels: Vec<usize>) -> (f64, f64) {
     let max_label = *(labels.iter().max().unwrap());
     let bincount: Vec<i64> = (0..=max_label)
         .into_iter()
@@ -67,4 +66,21 @@ pub fn permanova(sqdistances: PyReadonlyArray<f64, Ix2>, mut labels: Vec<usize>)
         f,
         other_fs.iter().filter(|&other_f| *other_f >= f).count() as f64 / other_fs.len() as f64,
     );
+}
+
+pub fn generate_data(size: usize, category_count: usize) -> (Array2<f64>, Vec<usize>) {
+    let mut distances = Array2::random((size, size), Uniform::new(0., 1.));
+    for i in 0..size {
+        for j in 0..i {
+            let total = distances[[i,j]] + distances[[j, i]];
+            distances[[i,j]] = total;
+            distances[[i,j]] = total;
+        }
+    }
+    for i in 0..size {
+        distances[[i,i]] = 0.;
+    }
+    let labels = Array1::random(size, Uniform::new(0, category_count));
+
+    (distances, labels.to_vec())
 }
