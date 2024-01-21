858e5da06235d3cf71a0edecf90cbbcbf18e4f8c
Iliya Malecki <iliyamalecki@gmail.com>
2024-01-21 23:40:11 +0100

    fixed globals() profiling problem

--- pytests/becnhes.py
+++ pytests/becnhes.py
@@ -3,12 +3,8 @@ from reference.square import permanova as square_permanova
 import cProfile
 import numpy as np
 
-def main():
-    dist = np.random.random((600,600))
-    dist = dist + dist.T
-    np.fill_diagonal(dist, 0)
-    labels = np.random.randint(0, 3, 600)
-    cProfile.run('square_permanova(dist, labels)', sort='tottime')
-
-if __name__ == '__main__':
-    main()
+dist = np.random.random((600,600))
+dist = dist + dist.T
+np.fill_diagonal(dist, 0)
+labels = np.random.randint(0, 3, 600)
+cProfile.run('square_permanova(dist, labels)', sort='tottime')
