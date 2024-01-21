08a0bbcfb12ea9479d810a8770a3c246a0c28142
Iliya Malecki <iliyamalecki@gmail.com>
2024-01-22 00:27:45 +0100

    fmt

--- pytests/becnhes.py
+++ pytests/becnhes.py
@@ -3,8 +3,9 @@ from reference.square import permanova as square_permanova
 import cProfile
 import numpy as np
 
-dist = np.random.random((600,600))
+size = 1000
+dist = np.random.random((size, size))
 dist = dist + dist.T
 np.fill_diagonal(dist, 0)
-labels = np.random.randint(0, 3, 600)
-cProfile.run('square_permanova(dist, labels)', sort='tottime')
+labels = np.random.randint(0, 3, size)
+cProfile.run("square_permanova(dist, labels)", sort="tottime")
