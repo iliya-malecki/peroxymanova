38f2b51277fde670faf260d5d9203deb5406ddaa
Iliya Malecki <iliyamalecki@gmail.com>
2024-01-24 01:27:29 +0100

    fixed my tests

--- pytests/tests.py
+++ pytests/tests.py
@@ -2,26 +2,27 @@ from skbio.stats.distance import permanova, DistanceMatrix
 from reference import square
 import peroxymanova
 import numpy as np
+from scipy.spatial import distance_matrix
+from scipy.stats import f_oneway
 
-size = 1000
-dist = np.random.random((size, size))
-dist = dist + dist.T
-np.fill_diagonal(dist, 0)
-labels = np.random.randint(0, 3, size)
-
+size = 10
+objects = np.random.random((size, 1))
+dist = distance_matrix(objects, objects)
+labels = np.random.randint(0, 2, size)
 standard = permanova(
     DistanceMatrix(dist),
-    labels,
+    labels.copy(),
     permutations=1000
 )
 
-python = square.permanova(dist, labels)
+python = square.permanova(dist**2, labels.copy())
 
-our = peroxymanova.permanova(dist, labels)
+our = peroxymanova.permanova(dist**2, labels.copy())
+anova = f_oneway(*(objects[labels == i] for i in np.unique(labels)))
 
 print(standard)
 print(python)
 print(our)
+print(anova)
 
-assert standard['test statistic'] == our[0]
-assert standard['p-value'] == our[1]
+assert np.allclose(standard['test statistic'], our[0])
