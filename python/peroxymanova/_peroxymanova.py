12000b6ce2d16c59f8687be87463cf2c509bafed
Iliya Malecki <iliyamalecki@gmail.com>
2024-02-08 01:57:34 +0100

    ditching scipy distance

--- python/peroxymanova/_peroxymanova.py
+++ python/peroxymanova/_peroxymanova.py
@@ -1,13 +1,11 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING, TypeVar, overload, cast
+from typing import TYPE_CHECKING, TypeVar, cast
 from ._oxide import permanova, ordinal_encoding
 import numpy as np
-from scipy.spatial.distance import pdist, squareform
 from typing import NamedTuple
 
 if TYPE_CHECKING:
     from typing import Collection, Callable, Literal, TypeGuard, Any
-    from ._scipy_types import _FloatValue, _MetricKind
 
 
 class PermanovaResults(NamedTuple):
@@ -18,7 +16,7 @@ class PermanovaResults(NamedTuple):
 T = TypeVar("T")
 
 
-def get_distance_matrix(things: Collection[T], distance: Callable[[T, T], _FloatValue]):
+def get_distance_matrix(things: Collection[T], distance: Callable[[T, T], np.float64]):
     dists = np.empty((len(things), len(things)), dtype=np.float64)
     for i, a in enumerate(things):
         for j, b in enumerate(things):
@@ -47,38 +45,19 @@ def pinky_promise_guard(
 
 def calculate_distances(
     things: Collection[T],
-    distance: Callable[[T, T], _FloatValue] | _MetricKind,
-    engine: str,
+    distance: Callable[[T, T], np.float64],
+    engine: Literal["python", "numba"],
 ) -> np.ndarray[np.floating[Any], Any]:
     if len(things) < 2:
         raise ValueError("len(things) < 2")
 
-    if engine == "scipy":
-        if isinstance(things, np.ndarray) and len(things.shape) == 2:
-            if callable(distance) or isinstance(distance, str):
-                # len(things.shape) == 2 ensures T is ndarray
-                return squareform(
-                    pdist(
-                        things,
-                        cast(
-                            "Callable[[np.ndarray, np.ndarray], _FloatValue] | _MetricKind",
-                            distance,
-                        ),
-                    )
-                )
-            else:
-                raise ValueError("distance wrong for engine == 'scipy'")
-        else:
-            raise ValueError("things wrong")
+    if not callable(distance):
+        raise ValueError("distance wrong")
 
-    elif engine == "python":
-        if isinstance(distance, str):
-            raise ValueError("distance wrong")
+    if engine == "python":
         return get_distance_matrix(things, distance)
 
     elif engine == "numba":
-        if isinstance(distance, str):
-            raise ValueError("distance wrong")
         import numba
 
         return numba.jit(get_distance_matrix)(things, distance)  # type: ignore
@@ -86,33 +65,13 @@ def calculate_distances(
     raise ValueError("engine wrong")
 
 
-@overload
 def run(
     things: Collection[T],
-    distance: Callable[[T, T], _FloatValue],
+    distance: Callable[[T, T], np.float64],
     labels: np.ndarray[np.str_ | np.int_, Any],
     engine: Literal["python", "numba"],
 ) -> PermanovaResults:
-    ...
-
-
-@overload
-def run(
-    things: np.ndarray[Any, Any],
-    distance: _MetricKind,
-    labels: np.ndarray[np.str_ | np.int_, Any],
-    engine: Literal["scipy"],
-) -> PermanovaResults:
-    ...
-
-
-def run(
-    things: Collection[T],
-    distance: Callable[[T, T], _FloatValue] | _MetricKind,
-    labels: np.ndarray[np.str_ | np.int_, Any],
-    engine: Literal["scipy", "python", "numba"],
-) -> PermanovaResults:
-    if labels.dtype is np.dtype("str"):
+    if labels.dtype is np.dtype(str):
         fastlabels = ordinal_encoding(cast("np.ndarray[np.str_, Any]", labels))
     else:
         if not (min(labels) == 0 and max(labels) == len(np.unique(labels)) - 1):
