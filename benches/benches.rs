051b8123c8135d68074d276023a6cc80bb16712c
Iliya Malecki <iliyamalecki@gmail.com>
2024-03-08 23:49:51 +0100

    added benches

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,10 +1,23 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::time::Duration;
 
-use peroxymanova::_permanova;
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+// use ndarray::prelude::*;
+use peroxymanova::{_permanova, generate_data};
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib 20", |b| b.iter(|| black_box(2) + 2));
+    let (data, labels) = black_box(generate_data(1000, 7));
+    c.bench_function("standard permanova", |b| {
+        b.iter_batched(
+            || labels.clone(),
+            |labels| _permanova(&data.view(), labels, 1000),
+            criterion::BatchSize::SmallInput,
+        )
+    });
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group! {
+    name = benches;
+    config = Criterion::default().measurement_time(Duration::from_secs(100));
+    targets = criterion_benchmark
+}
 criterion_main!(benches);
