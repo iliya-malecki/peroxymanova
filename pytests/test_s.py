17813d63d99a7eda1a99983317cde518c197681c
Iliya Malecki <iliyamalecki@gmail.com>
2024-04-23 01:47:42 +0200

    wip add extensive testing

--- pytests/test_s.py
+++ pytests/test_s.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from typing import Any, Generic, TypeVar
-from reference import square
 import peroxymanova
 import numpy as np
 from scipy.spatial import distance_matrix
@@ -73,3 +72,85 @@ def test_run_indexable(labeltype, symmetrification):
         symmetrification=symmetrification,
     )
     assert np.allclose(anova.statistic[0], run_py_results.statistic)
+
+
+@pytest.mark.parametrize("engine", ["python", "numba"])
+def test_clean_stdout(engine, capsys: pytest.CaptureFixture[str]):
+    peroxymanova.permanova_pipeline(
+        objects,
+        distance_function,
+        labels,
+        engine=engine,
+        symmetrification="roundtrip",
+    )
+    assert not capsys.readouterr().out
+
+
+@pytest.mark.parametrize("engine", ["python", "numba"])
+def test_original_data_intact(engine):
+    previousdist = dist.copy()
+    previouslables = labels.copy()
+    peroxymanova.permanova_pipeline(
+        objects,
+        distance_function,
+        labels,
+        engine=engine,
+        symmetrification="roundtrip",
+    )
+    assert np.allclose(previousdist, dist)
+    assert np.allclose(previouslables, labels)
+
+
+@pytest.mark.parametrize("ngroups", range(2, 10))
+def test_different_number_of_groups(ngroups):
+    labels = np.random.randint(0, ngroups, size)
+    our = peroxymanova.permanova(dist**2, labels.astype(np.uint))
+    anova = f_oneway(*(objects[labels == i] for i in np.unique(labels)))
+    assert np.allclose(our[0], anova.statistic[0])
+
+
+@pytest.mark.parametrize("npermutations", range(3))
+def test_different_number_of_permutations(npermutations):
+    labels = np.random.randint(0, 5, size)
+    our = peroxymanova.permanova(
+        dist**2, labels.astype(np.uint), permutations=npermutations * 10
+    )
+    anova = f_oneway(*(objects[labels == i] for i in np.unique(labels)))
+    assert np.allclose(our[0], anova.statistic[0])
+
+
+def test_graceful_value_errors():
+    with pytest.raises(ValueError):
+        peroxymanova.permanova(dist**2, np.zeros(size, dtype="int"))
+    with pytest.raises(ValueError):
+        peroxymanova.permanova(dist**2, np.zeros(size, dtype="uint"))
+    with pytest.raises(ValueError):
+        peroxymanova.permanova(
+            np.random.random((size, size + 1)), labels.astype("uint")
+        )
+    with pytest.raises(ValueError):
+        peroxymanova.calculate_distances(
+            [np.nan] * 100, lambda a, b: np.float64(a - b), "python", "roundtrip"
+        )
+
+
+@pytest.mark.parametrize(
+    "sillytype", [[42] * 3, (42,) * 3, False, None, object, Generic, lambda: 42]
+)
+def test_graceful_type_errors(sillytype):
+    with pytest.raises(TypeError):
+        peroxymanova.permanova(sillytype, labels.astype("uint"))
+    with pytest.raises(TypeError):
+        peroxymanova.permanova(dist, sillytype)
+
+    with pytest.raises(TypeError):
+        peroxymanova.ordinal_encoding(sillytype)
+
+    with pytest.raises(TypeError):
+        peroxymanova.calculate_distances(
+            sillytype, lambda a, b: np.float64(a - b), "python", "roundtrip"
+        )
+    with pytest.raises(TypeError):
+        peroxymanova.calculate_distances(
+            objects, lambda a, b: sillytype, "python", "roundtrip"
+        )
--- python/peroxymanova/__init__.py
+++ python/peroxymanova/__init__.py
@@ -1,3 +1,4 @@
+import importlib.metadata
 from ._oxide import permanova
 from ._peroxymanova import (
     calculate_distances,
@@ -5,3 +6,14 @@ from ._peroxymanova import (
     ordinal_encoding,
     PermanovaResults,
 )
+
+__all__ = [
+    "permanova",
+    "calculate_distances",
+    "permanova_pipeline",
+    "ordinal_encoding",
+    "PermanovaResults",
+]
+
+if __package__ is not None:
+    __version__ = importlib.metadata.version(__package__)
